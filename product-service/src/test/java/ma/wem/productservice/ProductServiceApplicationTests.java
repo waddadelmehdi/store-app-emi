8b7a8397415a3248394eab1c7e03e44e01ec7e96
WADDAD EL MEHDI <waddadelmehdi@gmail.com>
2024-11-18 03:07:59 +0100

    setup commit

---
+++ product-service/src/test/java/ma/wem/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.wem.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
